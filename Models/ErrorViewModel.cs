a9225d275536780756cc70fdf37c791eb7047a64
francis renauld <renauldf12@gmail.com>
2022-08-08 14:15:54 -0400

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Rocket_Elevator_Customer_Portal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
